3e2a476b677f957b4a5d8f0d27937141567b383b
m04j00 <m04j00@gmail.com>
2022-08-24 08:50:05 +0900

    #2 인덱서 문법 실습

---
+++ CsClass2/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsClass2
+{
+    internal class Products
+    {
+        public int this[int i]
+        {
+            get
+            {
+                return i;   
+            }
+            set
+            {
+                Console.WriteLine(i + "번째 상품 설정 : " + value);
+            }
+        }
+    }
+}
--- CsClass2/Program.cs
+++ CsClass2/Program.cs
@@ -10,13 +10,17 @@ namespace CsClass2
     internal class Program
     {
         static void Main(string[] args)
-    {
-        Wanted<string> wanstedString = new Wanted<string>("String");
-        Wanted<int> wantedInt = new Wanted<int>(12345);
-        Wanted<double> wantedDouble = new Wanted<double>(12.345);
-        Console.WriteLine(wanstedString.Value);
-        Console.WriteLine(wantedInt.Value);
-        Console.WriteLine(wantedDouble.Value);
+        {
+            Wanted<string> wanstedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(12345);
+            Wanted<double> wantedDouble = new Wanted<double>(12.345);
+            Console.WriteLine(wanstedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
+
+            Products p = new Products();
+            Console.WriteLine(p[4]);
+            p[4] = 5;
     }
     }
 }
