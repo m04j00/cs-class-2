9a8204594bfa0581b838d1af82275c0ce93594af
Min J <m04j00@gmail.com>
2022-09-02 22:03:59 +0900

    #6 인터페이스 - IComparable, IDisposal 실습

--- CsClass2/Program.cs
+++ CsClass2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSClass2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -63,6 +64,31 @@ namespace CsClass2
             Console.WriteLine(pointStructA.x + " / " + pointStructA.y);
             Console.WriteLine(pointStructB.x + " / " + pointStructB.y);
 
+
+            using (Dummy dummy = new Dummy())
+            {
+                List<Product> list = new List<Product>()
+                {
+                    new Product() { Name="고구마", Price=5000 },
+                    new Product() { Price=2400, Name="사과" },
+                    new Product() { Name="바나나", Price=2000 },
+                    new Product() { Name="배", Price=9000 },
+                };
+                list.Sort();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+
+        class Dummy : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose() 실행!");
+            }
         }
         struct Point
         {
